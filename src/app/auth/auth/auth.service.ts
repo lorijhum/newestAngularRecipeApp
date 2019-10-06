0eecfb3db1747e9a7b48759389cb1c4775c6eb39
Lori Humphries <lhemailme@aim.com>
2019-10-06 10:12:57 -0400

    adding user model and create user in auth service

--- src/app/auth/auth/auth.service.ts
+++ src/app/auth/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { catchError } from 'rxjs/operators';
-import { throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+import { throwError, Subject } from 'rxjs';
+import { User } from './user.model';
 
 
 export interface AuthResponseData {
@@ -14,6 +15,8 @@ export interface AuthResponseData {
 
 
 export class AuthService {
+    user = new Subject<User>();
+
     constructor(private http: HttpClient) {}
       
 
@@ -24,7 +27,12 @@ export class AuthService {
             password: password,
             returnSecureToken: true
         }
-        ).pipe(catchError(this.handleError));
+        ).pipe(catchError(this.handleError), 
+            tap(resData => {
+            this.handleAuthentication(resData.email, resData.localId, resData.idToken, +resData.expiresIn);   
+            
+        })
+        );
     }
 
     login(email: string, password: string) {
@@ -34,7 +42,26 @@ export class AuthService {
             password: password,
             returnSecureToken: true
         }
-        ).pipe(catchError(this.handleError));
+        ).pipe(catchError(this.handleError),
+            tap(resData => {
+            this.handleAuthentication(resData.email, resData.localId, resData.idToken, +resData.expiresIn);   
+            
+        })
+        );
+    }
+
+    private handleAuthentication(email: string, userId: string, token: string, expiresIn: number) {
+        const expirationDate = new Date(
+            new Date().getTime() + expiresIn * 1000
+            );
+        const user = new User(
+            email, 
+            userId, 
+            token, 
+            expirationDate
+            );
+        this.user.next(user);
+
     }
 
     private handleError(errorRes: HttpErrorResponse) {
