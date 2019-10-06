da1c8f4a20c2bcadd8b8bd132930cdfa0c323073
Lori Humphries <lhemailme@aim.com>
2019-10-06 09:00:46 -0400

    adding login authentication

--- src/app/auth/auth/auth.component.ts
+++ src/app/auth/auth/auth.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { AuthService } from './auth.service';
+import { AuthService, AuthResponseData } from './auth.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-auth',
@@ -28,9 +29,17 @@ export class AuthComponent implements OnInit {
     this.isLoading = true;
     const email = form.value.email;
     const password = form.value.password;
+    let authObs: Observable<AuthResponseData>;
     
     if(this.isLoginMode) {
-      //....
+      this.authService.login(email, password).subscribe(resData => {
+        console.log(resData);
+        this.isLoading = false;
+      }, errorMessage => {
+        console.log(errorMessage);
+        this.error = errorMessage;
+        this.isLoading = false;
+      })
     }else {
       this.authService.signup(email, password).subscribe(resData => {
         console.log(resData);
@@ -38,7 +47,7 @@ export class AuthComponent implements OnInit {
       }, errorMessage => {
         console.log(errorMessage);
         this.error = errorMessage;
-          this.isLoading = false;
+        this.isLoading = false;
         
       });
      
--- src/app/auth/auth/auth.service.ts
+++ src/app/auth/auth/auth.service.ts
@@ -3,16 +3,19 @@ import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
 
 
-interface AuthResponseData {
+export interface AuthResponseData {
     idToken: string;
     email: string;
     refreshToken: string;
     expiresIn: string;
     localId: string;
+    registered?: boolean;
 }
+
 
 export class AuthService {
     constructor(private http: HttpClient) {}
+      
 
     signup(email: string, password: string) {
         return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBjugYEuOTGbRkENSU3snI8ctqBl5FEAOM',
@@ -29,6 +32,28 @@ export class AuthService {
             switch (errorRes.error.error.message) {
                 case 'EMAIL_EXISTS':
                   errorMessage = 'This Email already exists'
+              }
+              return throwError(errorMessage);
+        }));
+    }
+
+    login(email: string, password: string) {
+        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBjugYEuOTGbRkENSU3snI8ctqBl5FEAOM ',
+        {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        }
+        ).pipe(catchError(errorRes => {
+            let errorMessage = 'An unknown error occured';
+            if(!errorRes.error || !errorRes.error.error) {
+                return throwError(errorMessage);
+            }
+            switch (errorRes.error.error.message) {
+                case 'EMAIL_NOT_FOUND':
+                  errorMessage = 'This Email does not exist'
+                 case 'INVALID_PASSWORD':
+                    errorMessage = 'Wrong password entered'  
               }
               return throwError(errorMessage);
         }));
