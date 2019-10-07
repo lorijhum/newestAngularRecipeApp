aad75cb047e9c40c56138869948eb52e4789e06d
Lori Humphries <lhemailme@aim.com>
2019-10-07 13:41:55 -0400

    adding logging out

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -37,5 +37,7 @@ export class HeaderComponent implements OnInit, OnDestroy {
         this.dataStorageService.fetchRecipes().subscribe();
     }
   
-   
+   onLogout() {
+       this.authService.logout();
+   }
 }
